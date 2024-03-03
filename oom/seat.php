a85a22ebffab784176ad38f3ed081ee12e1fb71e
JCBen123 <161997792+JCBen123@users.noreply.github.com>
2024-03-03 13:31:50 +0800

    Update seat.php

--- oom/seat.php
+++ oom/seat.php
@@ -3,8 +3,17 @@
         <title> Hotpot Booking System </title>
         <link rel="stylesheet" href="oom.css">
         <h1>Hotpot Booking System</h1>
+        <h3> Choose your seat </h3>
     </head>
 
-    
+    <div class="cont1">
 
+        <p style="text-align: center;">wtf do i do</p>
+
+    </div>
+    <div class="cont2">
+        <form action="payment.php">
+            <input type="submit" id="Submit" value="Next">
+        </form>
+    </div>
 </html>
