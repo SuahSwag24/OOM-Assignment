d6e07a326eb2ad3a8511a54364844180edcae772
JCBen123 <161997792+JCBen123@users.noreply.github.com>
2024-03-03 13:32:10 +0800

    Update payment.php

--- oom/payment.php
+++ oom/payment.php
@@ -3,6 +3,7 @@
         <title> Hotpot Booking System </title>
         <link rel="stylesheet" href="oom.css">
         <h1>Hotpot Booking System</h1>
+        <h3> Choose your payment method </h3>
     </head>
 
     
