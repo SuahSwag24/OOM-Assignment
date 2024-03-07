90c9f45462e66a253c82187b817801d7a022f0b6
JCBen123 <161997792+JCBen123@users.noreply.github.com>
2024-03-07 15:52:36 +0800

    Changing this file from the user info the the middle page

--- oom/booking.php
+++ oom/booking.php
@@ -1,16 +1,10 @@
 <?php
+$page=isset($_GET["action"])?$_GET["action"]:"";
 
-    include "Class.php";
-    $url = "package.php";
-
-    if(isset($_POST['Submit']))
-    {
-        $customer = new Customer($_POST['name'] , $_POST['gender'] , $_POST['age'] , $_POST['pax'] , $_POST['phone'] , $_POST['email']);
-        header('Location: ' . $url);
-    }
-
+if($page=="Back"){
+    header("location:info.php");
+}
 ?>
-
 
 <html>
     <head>
@@ -18,37 +12,20 @@
         <link rel="stylesheet" href="oom.css">
         <h1>Hotpot Booking System</h1>
     </head>
-    <div class="form">
-    <form action="booking.php" method="post">
-        <table>
-                <tr>
-                    <th><label for="name">Name:</label></th>
-                    <td><input type="text" name="name" placeholder="Enter Name..." ></td>
-                </tr>
-                <tr>
-                    <th><label for="age">Age:</label></th>
-                    <td><input type="number" name="age" min="1" placeholder="Enter Age..." ></td>
-                </tr>
-                <tr>
-                    <th><label for="gender">Gender:</label></th>
-                    <td><input type="radio" name="gender" value="male" >Male <input type="radio" name="gender" value="female">Female</td>
-                </tr>
-                <tr>
-                    <th><label for="email">Email:</label></th>
-                    <td><input type="text" name="email" placeholder="Enter Email..." ></td>
-                </tr>
-                <tr>
-                    <th><label for="phone">Phone Number:</label></th>
-                    <td><input type="text" name="phone" placeholder="Enter Phone Number..." ></td>
-                </tr>
-                <tr>
-                    <th><label for="pax">Pax:</label></th>
-                    <td><input type="number" name="pax" min="1" placeholder="Enter Number of People..." ></td>
-                </tr>
-                <tr>
-                    <td colspan="2"><input type="submit" name="Submit" value="Proceed" ></td>
-                </tr>
-        </table>
-    </form>
+
+    <div class="bookingCont" style="background-color: aquamarine">
+        <p><a href="package.php"><b> New Booking </b></a></p>
     </div>
+    <br>
+
+    <div class="bookingCont" style="background-color: aquamarine">
+        <p><a href="recentBookings.php"><b> View Recent Bookings </b></a></p>
+    </div>
+    
+    <div class="cont2">
+        <form action="booking.php" method="get">
+            <input type="submit" name="action" id="Back" value="Back" onclick="send()" formnovalidate>
+        </form>
+    </div>
+
 </html>
