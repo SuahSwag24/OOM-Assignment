5bb54634fe27413df151f64b638bd7b61d9db8fc
JCBen123 <161997792+JCBen123@users.noreply.github.com>
2024-03-07 15:54:44 +0800

    Update payment.php

--- oom/payment.php
+++ oom/payment.php
@@ -1,3 +1,13 @@
+<?php
+$page=isset($_GET["action"])?$_GET["action"]:"";
+
+if($page=="Back"){
+    header("location:seat.php");
+}else if($page=="Next"){
+    header("location:validate.php");
+}
+?>
+
 <html>
     <head>
         <title> Hotpot Booking System </title>
@@ -6,6 +16,15 @@
         <h3> Choose your payment method </h3>
     </head>
 
+
     
+    <div class="cont2">
+        <form action="seat.php" method="get">
+            <form action="validate.php" method="post">
+                <input type="submit" name="action" id="Back" value="Back" onclick="send()" formnovalidate>    
+                <input type="submit" name="action" id="Next" value="Next" onclick="send()">
+            </form>
+        </form>
+    </div>
 
 </html>
