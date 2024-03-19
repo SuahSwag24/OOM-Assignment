bb7bb5e6bd8e854c51386f2435be2587d3c55dc6
SuahSwag24 <suahswag24@gmail.com>
2024-03-19 21:28:07 +0800

    Re-commit due to desync

--- oom/payment.php
+++ oom/payment.php
@@ -18,21 +18,77 @@
         <title> Hotpot Booking System </title>
         <link rel="stylesheet" href="oom.css">
         <h1>Hotpot Booking System</h1>
-        <h3> Choose your payment method </h3>
     </head>
-    
-    <body>
-        <div class="pcont">
-            <div class="box" id="1">
-                <p>Method 1</p>
-            </div>
 
-            <div class="space"></div>
-
-            <div class="box" id="2">
-                <p>Method 2</p>
-            </div>
+    <div class="pcont">
+        <h3> 1. Choose your payment type </h3>
+        <div class="wrapper">
+            <input type="radio" name="select" id="option-1">
+            <input type="radio" name="select" id="option-2">
+            <label for="option-1" class="option option-1">
+                <div class="dot"></div>
+                <span>Pay in full online</span>
+            </label>
+            <label for="option-2" class="option option-2">
+                <div class="dot"></div>
+                <span>Book now and pay later</span>
+            </label>
         </div>
+    </div>
+
+    <br><br>
+
+    <div class="pcont">
+        <h3> 2. Choose your payment method </h3>
+        <div class="wrapper">
+        <form action="validate.php">
+            <label for="paymentMtd">Payment:</label>
+            <select name="paymentMethod" id="paymentMethod">
+                <option value="visa">Visa Card</option>
+                <option value="master">Master Card</option>
+                <option value="paypal">Paypal</option>
+                <option value="touchNGo">Touch N' Go</option>
+                <option value="cash">Cash</option>
+                <option value="cash">Other</option>
+            </select>
+        </form>
+    </div>
+
+    <br><br>
+
+    <div class="pcont">
+        <h3> 3. Payment details </h3>
+        <div class="wrapper">
+            <ul>
+                <label for="packageID">Package ID: </label>
+                <br><br>
+                <label for="displayPackageDetails">Package Details: </label>
+                <br><br>
+                <label for="totalPrice">Total Price: </label>
+            </ul>
+    </div>
+
+<!-- Previous Code Template - Please delete if not used.
+    <div class="pcont">
+        <div class="wrapper" id="1">
+        <div class="dot"></div>
+            <p>Method 1</p>
+            <input type="radio" name="paymentType" id="payment" value="payNow"><br>
+            <label for = "option-1" class="radio">Pay in full online</label>
+        </div>
+        </div>
+
+        <div class="space"></div>
+        
+        <div class="wrapper" id="2">
+        <div class="dot"></div>
+            <p>Method 2</p>
+            <input type="radio" name="paymentType" id="payment" value="payLater"><br>
+            <label for = "option-2" class="radio">Book now and pay later</label>
+        </div>
+        </div>
+    </div>
+-->
 
         <div class="cont2">
             <form action="payment.php" method="get">
